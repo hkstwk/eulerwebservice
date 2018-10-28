c1eecf5454c143a2c02dd09fdea68818b5e8b21b
hkstwk <hkolvoort+github@gmail.com>
2018-10-28 16:51:27 +0100

    Small changes

--- EulerWebService/src/main/java/nl/hkolvoort/api/Euler001Resource.java
+++ EulerWebService/src/main/java/nl/hkolvoort/api/Euler001Resource.java
@@ -21,49 +21,46 @@ import nl.hkolvoort.euler.Euler001SumOfMultiples;
 import nl.hkolvoort.euler.Euler001ResponseBody;
 
 @Path("/euler/1")
-public class Euler001Resource extends Application{
+public class Euler001Resource extends Application {
 	
-/*	
- * URI: http://localhost:8080/EulerWebService/api/euler/1
- * sample json payload: {"multiple1":3, "multiple2":5,"limit":10}
- */
+	/*
+	 * URI: http://localhost:8080/EulerWebService/api/euler/1 sample json payload:
+	 * {"multiple1":3, "multiple2":5,"limit":10}
+	 */
 	
 	@POST
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response getSimpleObject2(Euler001RequestBody Euler001ReqBody) 
-	{
-	   // Do validations
-	   ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-	   Validator validator = factory.getValidator();
-	   Set<ConstraintViolation<Euler001RequestBody>> violations = validator.validate(Euler001ReqBody);
-	   
-	   // No violations found
-	   if  (violations.isEmpty()) 
-	   {
-		   // determine sum of multiples using input provided in Request Body
-		   Integer sum = Euler001SumOfMultiples.sumOfMultiples(Euler001ReqBody.getMultiple1(), Euler001ReqBody.getMultiple2(), Euler001ReqBody.getLimit());
-		   //Create response body
-		   Euler001ResponseBody resp = new Euler001ResponseBody(Euler001ReqBody.getMultiple1(),Euler001ReqBody.getMultiple2(), Euler001ReqBody.getLimit(), sum);
-		   
-		   return Response.status(Response.Status.OK).entity(resp).build();
-	   }	
-	   // One or more violation found
-	   else 
-	   {
-		   // create response body
-		   EulerValidationMessages ev = new EulerValidationMessages();
-		   for (ConstraintViolation<Euler001RequestBody> violation : violations) 
-		   {
-			   ev.add(new Euler001ValidationMessage(
-					   		violation.getRootBeanClass().toString(),
-					   		violation.getPropertyPath().toString(),
-					   		violation.getInvalidValue().toString(),
-					   		violation.getMessage().toString()
-					   ));
-		   }
-		   
-		   return Response.status(Response.Status.BAD_REQUEST).entity(ev).build();
-	   }
+	public Response getSimpleObject2(Euler001RequestBody Euler001ReqBody) {
+		
+		// Do validations
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<Euler001RequestBody>> violations = validator.validate(Euler001ReqBody);
+
+		// No violations found
+		if (violations.isEmpty()) {
+			// determine sum of multiples using input provided in Request Body
+			Integer sum = Euler001SumOfMultiples.sumOfMultiples(Euler001ReqBody.getMultiple1(),
+					Euler001ReqBody.getMultiple2(), Euler001ReqBody.getLimit());
+			// Create response body
+			Euler001ResponseBody resp = new Euler001ResponseBody(Euler001ReqBody.getMultiple1(),
+					Euler001ReqBody.getMultiple2(), Euler001ReqBody.getLimit(), sum);
+
+			return Response.status(Response.Status.OK).entity(resp).build();
+		}
+		
+		// One or more violation found
+		else {
+			// create response body
+			EulerValidationMessages ev = new EulerValidationMessages();
+			for (ConstraintViolation<Euler001RequestBody> violation : violations) {
+				ev.add(new Euler001ValidationMessage(violation.getRootBeanClass().toString(),
+						violation.getPropertyPath().toString(), violation.getInvalidValue().toString(),
+						violation.getMessage().toString()));
+			}
+
+			return Response.status(Response.Status.BAD_REQUEST).entity(ev).build();
+		}
 	}
 }
